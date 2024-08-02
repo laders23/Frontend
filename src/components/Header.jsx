0217607290ef33bdadf14849974465fe1316def1
PANdas <krsy0411@naver.com>
2024-08-03 04:55:43 +0900

    로그인 모달을 메인에 합치기 위해 메인 페이지 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,35 +4,47 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
+import { useReducer } from "react";
+import LoginModal from "./LoginModal";
 
 function Header() {
+	const [isModalOpen, modalToggle] = useReducer(
+		(modalState) => !modalState,
+		false
+	);
+
 	return (
-		<header className="headerContainer">
-			<ul className="ulContainer">
-				<div className="logoAndsearch">
-					<Logo />
-					<div className="searchBar__container">
-						<FontAwesomeIcon icon={faMagnifyingGlass} />
-						<input
-							className="searchBar"
-							type="text"
-							placeholder="나의 인생 선배를 찾아보세요"
-						/>
+		<>
+			<header className="headerContainer">
+				<ul className="ulContainer">
+					<div className="logoAndsearch">
+						<Logo />
+						<div className="searchBar__container">
+							<FontAwesomeIcon icon={faMagnifyingGlass} />
+							<input
+								className="searchBar"
+								type="text"
+								placeholder="나의 인생 선배를 찾아보세요"
+							/>
+						</div>
 					</div>
-				</div>
-				<div className="linkBox">
-					<li className="linkItem">
-						<Link to={"/"}>로그인</Link>
-					</li>
-					<li className="linkItem">
-						<Link to={"/"}>회원가입</Link>
-					</li>
-					<li className="linkItem">
-						<Link to="/mypage">마이페이지</Link>
-					</li>
-				</div>
-			</ul>
-		</header>
+					<div className="linkBox">
+						<li className="linkItem">
+							<div className="linkItem__login" onClick={modalToggle}>
+								로그인
+							</div>
+						</li>
+						<li className="linkItem">
+							<Link to={"/"}>회원가입</Link>
+						</li>
+						<li className="linkItem">
+							<Link to="/mypage">마이페이지</Link>
+						</li>
+					</div>
+				</ul>
+			</header>
+			<LoginModal isModalOpen={isModalOpen} modalToggle={modalToggle} />
+		</>
 	);
 }
 export default Header;
