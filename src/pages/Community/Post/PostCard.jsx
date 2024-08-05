fb05f14775f4a616d6235208be4b923325f193ed
PANdas <krsy0411@naver.com>
2024-08-06 02:10:32 +0900

    커뮤니티 : PostDetail 레이아웃 및 스타일 약간의 오류 수정 완료

--- src/pages/Community/Post/PostCard.jsx
+++ src/pages/Community/Post/PostCard.jsx
@@ -1,14 +1,14 @@
+/* eslint-disable react/prop-types */
 // src/pages/Community/Post/PostCard.jsx
-import React from 'react';
-import './PostCard.css'; // 필요한 스타일 파일 import
+import "./PostCard.css"; // 필요한 스타일 파일 import
 
 const PostCard = ({ post }) => {
-    return (
-        <div className="post-card">
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
-        </div>
-    );
+	return (
+		<div className="post-card">
+			<h2>{post.title}</h2>
+			<p>{post.content}</p>
+		</div>
+	);
 };
 
 export default PostCard;
--- src/pages/Community/Post/PostList.jsx
+++ src/pages/Community/Post/PostList.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import PostCard from './PostCard';
+/* eslint-disable react/prop-types */
+import PostCard from "./PostCard";
 
 const PostList = ({ posts, onPostClick }) => {
-    return (
-        <div>
-            {posts.map(post => (
-                <div key={post.id} onClick={() => onPostClick(post.id)}> {/* 클릭 핸들러 추가 */}
-                    <PostCard post={post} />
-                </div>
-            ))}
-        </div>
-    );
+	return (
+		<div>
+			{posts.map((post) => (
+				<div key={post.id} onClick={() => onPostClick(post.id)}>
+					{" "}
+					{/* 클릭 핸들러 추가 */}
+					<PostCard post={post} />
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default PostList;
